aac8c4a4c2c4feee819148182e343ac74a59f0b1
Kristit <kristitammet@gmail.com>
2017-02-23 15:13:13 +0200

    Add files via upload
    Form2- PHP file, et võtta info sisse From 2 failist

---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+
+    $user=  $_POST['name'];
+    $adr=   $_POST['street'];
+    $zipp=  $_POST['zip'];
+    $count= $_POST['country'];
+    $paying=$_POST['paying method'];
+    $sme=   $_POST['message'];
+echo "Cleints name is $user<br> aadress is: $adr<br> $zipp $count<br>preferred paying method is: $paying and message for the seller is: $sme"
+?>
+    
+
